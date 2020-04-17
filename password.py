454be65f835cf4c9306dfc3a77e19844912e985b
Essy Mwangi <sonnieessy@gmail.com>
2020-04-17 13:02:19 +0300

    used  __init__ method helps us define propeties for our object and created an empty user list

--- password.py
+++ password.py
@@ -2,4 +2,17 @@ class User:
   '''
   A Class that generates new instances of a user
   '''
-  
+  user_list = [] #Empty user list
+
+  def __init__(self,username,password):
+    '''
+    __init__ method helps us define propeties for our objects
+    Ars:
+      username: New User username.
+      passors: New Use password.
+    '''
+    self.username = username
+    self.password = password
+
+
+    
