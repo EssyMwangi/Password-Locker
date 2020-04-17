8696512d8254ad4f9d657d7da0c6743f782851df
Essy Mwangi <sonnieessy@gmail.com>
2020-04-17 13:05:49 +0300

    a test_save_user test case to test if the user object is saved into the user list

--- password_test.py
+++ password_test.py
@@ -21,3 +21,9 @@ class TestUser(unittest.TestCase):
     self.assertEqual(self.new_user.username,"EssyMwangi")
     self.assertEqual(self.new_user.password,"Monica/1")
   
+  def test_save_user(self):
+    '''
+    test_save_user test case to test if the user object is saved into the user list
+    '''
+    self.new_user.save_user()
+    self.assertEqual(len(User.user_list),1)
