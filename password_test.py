ce4485baeabef902e717549752c247123e7860ac
Essy Mwangi <sonnieessy@gmail.com>
2020-04-17 13:04:50 +0300

    Created Test class that defines test cases for User class behaviors,a Set up method to run before each test cases and  test__init test case to test if the object is initialized properly

--- password_test.py
+++ password_test.py
@@ -1,2 +1,23 @@
 import unittest
 from password import User
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines test cases for User class behaviors
+  Args:
+  unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+  def setUp(self):
+    '''
+    Set up method to run before each test cases
+    '''
+    self.new_user = User( "EssyMwangi","Monica/1")
+
+  def test_init(self):
+    '''
+    test__init test case to test if the object is initialized properly
+    '''
+
+    self.assertEqual(self.new_user.username,"EssyMwangi")
+    self.assertEqual(self.new_user.password,"Monica/1")
+  
